fb5380bdbda486a6917d104e94f4d2016438448f
belljun3395 <195850@jnu.ac.kr>
2022-06-12 19:44:33 +0900

    [study] 15.1.7 까지

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 var createError = require('http-errors'),
     express = require('express'),
     path = require('path'),
-    logger = require('morgan'),
+    morgan = require('morgan'),
     cookieParser = require('cookie-parser'),
     sequelize = require('./sequelize/models').sequelize,
     session = require('express-session'),
     bodyParser = require('body-parser'),
-    passport = require('passport');
+    passport = require('passport'),
+    helmet = require('helmet'),
+    hpp = require('hpp');
 
 const { swaggerUi, specs } = require('./src/modules/swagger');
 const sign = require('./src/function/checkAPIKey');
+const logger = require('./logger/logger.js');
+
 // add config  
 var dotenv = require('dotenv'),
     passportConfig = require('./passport');
@@ -35,11 +39,17 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '/src/public')));
 
 // middleware  
-app.use(logger('dev'));
+if(process.env.NODE_ENV==='production'){
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(hpp());
+} else {
+  app.use(morgan('dev'));
+}
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
+const sessionOption = {
   resave : false,
   saveUninitialized : false,
   secret : process.env.SESSION_SECRET,
@@ -47,18 +57,30 @@ app.use(session({
     httpOnly : true,
     secure : false,
   },
-}));
+}
+if(process.env.NODE_ENV==='production'){
+  sessionOption.proxy=true;
+  sessionOption.cookie.secure=true;
+}
+app.use(session(sessionOption));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // routerMiddleware  
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.use(sign.checkApiKey);
-
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', sign.checkApiKey);
+
+app.use((req,res,next) => { 
+  const err = new Error('NotFound');
+  err.status=404;
+  logger.info('hello');
+  logger.error(err.message);
+  next(err);
+});
 
 // catch 404 and forward to error handler  
 app.use(function(req, res, next) {
